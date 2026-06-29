196101b5206fce4443f389f64d90f649f3f2ac17
Eduard Bagdasaryan <dictiano@gmail.com>
2026-06-29 18:55:46 +0300

    Update tests.h

--- tests.h
+++ tests.h
@@ -1,6 +1,6 @@
 #ifndef TEST_DOT_H
 #define TEST_DOT_H
-#include "generator.h"
+#include "generator.h" 
 
 //Contains some helper testing methods for char sequence data generation ( StrGenerator)          
 class BruteTester
