31a33f92d11cd011574f03087da7f036197c7ab6
Eduard Bagdasaryan <dictiano@gmail.com>
2026-08-01 18:24:27 +0300

    Update mk.bat

--- mk.bat
+++ mk.bat
@@ -1,4 +1,5 @@
 @echo off 
 
 REM a build script
-g++ -o brute.exe main.cpp generator.cpp tests.cpp argparser.cpp brute.cpp -std=c++11 -pthread -Lc:\MinGW\lib     
+g++ -o brute.exe main.cpp generator.cpp tests.cpp argparser.cpp brute.cpp -std=c++11 -pthread -Lc:\MinGW\lib  
+REM a build script another comment
